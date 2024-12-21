b6c3b7441ba91d8062196d1520f7a8a2c3720818
horsewin <mamamatz@gmail.com>
2024-12-21 21:10:07 +0900

    fix: handle api response

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,16 +4,20 @@ import { Welcome } from "../welcome/welcome";
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "sample web app" },
-    { name: "description", content: "Welcome to v2" }
+    { name: "description", content: "Welcome to v2" },
   ];
 }
 
 export async function loader() {
-  const response = await fetch(
-    "https://ebf4qapz35.execute-api.ap-southeast-2.amazonaws.com/prod/helloworld"
-  );
-  const data = await response.json();
-  return { message: data.message };
+  try {
+    const response = await fetch(
+      "https://ebf4qapz35.execute-api.ap-southeast-2.amazonaws.com/prod/helloworld"
+    );
+    const data = await response.json();
+    return { message: data.message };
+  } catch (error) {
+    return { message: "Hello, API response cannot be used" };
+  }
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
