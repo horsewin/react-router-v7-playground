24c4d93585e720725aa4bdc5bc03a43541e413d6
horsewin <mamamatz@gmail.com>
2025-08-24 22:37:40 +0900

    update

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -4,51 +4,34 @@ import { config } from "~/lib/config";
 export function meta() {
   return [
     { title: "sample web app" },
-    { name: "description", content: "Welcome to v2" }
+    { name: "description", content: "Welcome to v2" },
   ];
 }
 
 export async function loader() {
   let dataBackendUrl = null;
-  let dataServiceConnect = null;
   try {
     const responseBackendUrl = await fetch(
-      `${config.api.backendUrl}/v1/helloworld/error`
-    ).catch(error => {
+      `${config.api.backendUrl}/v1/helloworld/error`,
+    ).catch((error) => {
       console.error("Error fetching data:", error);
       dataBackendUrl = {
-        data: { message: "about info is not found" }
-      };
-    });
-    const responseServiceConnect = await fetch(
-      `${config.api.serviceConnectUrl}/v1/helloworld/error`
-    ).catch(error => {
-      console.error("Error fetching data for service connect:", error);
-      dataServiceConnect = {
-        data: {
-          message: "about info is not found for service connect"
-        }
+        data: { message: "about info is not found" },
       };
     });
 
     if (responseBackendUrl instanceof Response && !dataBackendUrl) {
       dataBackendUrl = await responseBackendUrl.json();
     }
-    if (responseServiceConnect instanceof Response && !dataServiceConnect) {
-      dataServiceConnect = await responseServiceConnect.json();
-    }
   } catch (error) {
     console.error("some error occurred", error);
     return {
       message: dataBackendUrl?.data.message ?? "about info is not found",
-      messageServiceConnect:
-        dataServiceConnect?.data.message ?? "about info is not found"
     };
   }
 
   return {
     message: dataBackendUrl.data.message,
-    messageServiceConnect: dataServiceConnect.data.message
   };
 }
 
